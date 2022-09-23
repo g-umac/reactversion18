addc69d9208325a6717aeca94bcac3cfdaff1c5b
g-umac <gamzeumac49@gmail.com>
2022-09-23 14:02:13 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component, Suspense } from 'react';
 import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
-
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Navbar from './components/Navbar';
 // use hoc for class based components
 class LegacyWelcomeClass extends Component {
   render() {
@@ -32,13 +34,16 @@ function Page() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <Welcome />
         <button type="button" onClick={() => changeLanguage('de')}>
           de
         </button>
         <button type="button" onClick={() => changeLanguage('en')}>
           en
+        </button>
+        <button type="button" onClick={() => changeLanguage('tr')}>
+          tr
         </button>
       </div>
       <div className="App-intro">
@@ -59,9 +64,119 @@ const Loader = () => (
 
 // here app catches the suspense from page in case translations are not yet loaded
 export default function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
   return (
     <Suspense fallback={<Loader />}>
-      <Page />
+   <Navbar/>
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+
+      options={{
+        "fullScreen": {
+            "enable": true,
+            "zIndex": -1
+        },
+        "particles": {
+            "number": {
+                "value": 10,
+                
+            },
+            "color": {
+                "value": "#FFFF00"
+            },
+            "shape": {
+                "type": "star",
+                "options": {
+                    "sides": 5
+                }
+            },
+   
+            "rotate": {
+                "value": 0,
+                "random": true,
+                "direction": "clockwise",
+                "animation": {
+                    "enable": true,
+                    "speed": 5,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 600,
+                "color": "#FFFF00",
+                "opacity": 0.4,
+                "width": 2
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": ["grab"]
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "bubble"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+        "background": {
+            "color": "#111",
+            "image": "",
+            "position": "50% 50%",
+            "repeat": "no-repeat",
+            "size": "cover"
+        }
+    }}
+    />
     </Suspense>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+const Navbar = () => {
+  return (
+    <Nav
+    className="justify-content-center"
+    activeKey="/home"
+    onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+  >
+    <Nav.Item >
+      <Nav.Link  className='text-warning' href="/home">Home</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link  className='text-warning' eventKey="link-1">About</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link  className='text-warning' eventKey="link-2">Project</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link  className='text-warning' eventKey="link-3"  >
+        Skills
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link  className='text-warning' eventKey="link-4">Contact</Nav.Link>
+    </Nav.Item>
+  </Nav>  )
+}
+
+export default Navbar

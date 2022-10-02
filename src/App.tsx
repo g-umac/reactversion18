9e97397b7b36768ee25d3b2b64c02573abfbbfd1
g-umac <100878962+g-umac@users.noreply.github.com>
2022-10-02 22:33:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,6 @@ import Home from './components/Home';
  
 function App() {
 
-/*  const getData = async () => {
-    const key = "8d5ebbde6faf0389f5535db79b3b891b"
-    const data = await axios(`https://api.themoviedb.org/3/movie/550?api_key=8d5ebbde6faf0389f5535db79b3b891b`);
-    console.log(data.data);
-  }
-  useEffect(() => {
-    getData()
-
-  }, [])  */  
-
   return (
     <div className="App">
       <header className="App-header">

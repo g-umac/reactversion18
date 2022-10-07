cbab1286bcd06700a84603436d6892864f34f6ec
g-umac <gamzeumac49@gmail.com>
2022-10-07 22:45:00 +0200

    react router 6 added in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
-
+import ErrorPage from "./components/Error/ErrorPage";
  
 function App() {
 
   return (
     <div className="App">
+         <NavBar/>
+ <Router>
       
-        <NavBar/>
-        <Home/>
-     </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+         
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <div> Foooter </div>
+    </Router>
+     
+      </div>
   );
 }
 

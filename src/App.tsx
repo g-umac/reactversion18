1f857a879983038bcf43ec68a6bc8fd88a420c60
g-umac <gamzeumac49@gmail.com>
2022-10-07 23:18:45 +0200

    new component added for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import ErrorPage from "./components/Error/ErrorPage";
- 
+import Movies from "./components/Movies/Movies";
+import MyMoviesList from "./components/MyMoviesList/MyMoviesList";
+
 function App() {
 
   return (
-    <div className="App">
-         <NavBar/>
- <Router>
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-         
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <div> Foooter </div>
-    </Router>
-     
-      </div>
+    <div className="App"> 
+      <Router>
+        <NavBar /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/mymovieslist" element={<MyMoviesList />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router> 
+    </div>
   );
 }
 
---
+++ src/components/MyMoviesList/MyMoviesList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyMoviesList = () => {
+  return (
+    <div>MyMoviesList</div>
+  )
+}
+
+export default MyMoviesList

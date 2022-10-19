8fcd13679affe23617d98e2e1779f9da8adb7abc
g-umac <gamzeumac49@gmail.com>
2022-10-19 15:41:25 +0200

    conflickt

--- src/interface/IMoviesState.interface.ts
+++
@@ -1,7 +0,0 @@
-import GetAllMoviesState from './GetAllMovies.interface';
-
-export default interface IMoviesState {
-    allMovies: GetAllMoviesState[],
-    movies: [] | any, 
-    loading: Boolean,
-}
